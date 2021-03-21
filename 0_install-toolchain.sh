59c4f2329995f46b74a56fcc19206b1d076d6908
Jörg Bauder <34174944+jb0000@users.noreply.github.com>
2021-03-21 14:37:12 +0100

    works now

--- 0_install-toolchain.sh
+++ 0_install-toolchain.sh
@@ -14,8 +14,14 @@ dpkg-reconfigure locales
 # Install basic development tools
 #sudo dpkg --add-architecture i386
 sudo apt-get update -qq
-sudo apt install unzip git gdb curl cmake libx11-dev libglu1-mesa-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev zlib1g-dev libasound2-dev libgtk2.0-dev libjack-jackd2-dev jq
+sudo apt-get -y install unzip git gdb curl cmake libx11-dev libglu1-mesa-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev zlib1g-dev libasound2-dev libgtk2.0-dev libjack-jackd2-dev jq
 sudo locale-gen UTF-8
+
+# Update compilers
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get -y install gcc-6 g++-6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 20 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 
 # Get Rack SDK
 cd /home/vagrant
@@ -23,5 +29,5 @@ wget -nv https://vcvrack.com/downloads/Rack-SDK-1.1.6.zip
 unzip Rack-SDK-1.1.6.zip
 rm Rack-SDK-1.1.6.zip
 
-echo "export RACK_DIR=/home/vagrant/Rack-SDK-1.1.6" >> /home/vagrant/.bashrc
+echo "export RACK_DIR=/home/vagrant/Rack-SDK" >> /home/vagrant/.bashrc
 
